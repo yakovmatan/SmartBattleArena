3a8ad38739ec81e82e4e9d331fd4d2033490bc18
yakov matan <yakovmatan@gmail.com>
2025-05-20 15:38:47 +0300

    create class battle robot

---
+++ BattleRobot.cs
@@ -0,0 +1,18 @@
+﻿public class BattleRobot : IPowerable , IAttackable, IMovable
+{
+    public bool IsOn { get; set; } = false;
+
+    
+    public void TurnOn()
+    {
+        IsOn = true;
+    }
+
+    public void TurnOff()
+    {
+        IsOn = false;
+    }
+
+
+
+}
